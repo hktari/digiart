f00dab7a9191d08048f0df2a91cc4ce8029c890e
Boštjan Kamnik <bkamnik1995@gmail.com>
2026-05-06 10:57:18 +0200

    add migration

---
+++ apps/mvp/prisma/migrations/20260506085650_mvp_06_05_2026/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "CollectorProfile" ADD COLUMN     "shippingAddressLine1" TEXT,
+ADD COLUMN     "shippingAddressLine2" TEXT,
+ADD COLUMN     "shippingCity" TEXT,
+ADD COLUMN     "shippingName" TEXT,
+ADD COLUMN     "shippingZip" TEXT;
